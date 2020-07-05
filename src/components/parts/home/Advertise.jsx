3708e0e1a333158445497f216f21353f2db5505c
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-05 15:00:19 +0800

    Import parts -> home files in home component

---
+++ src/components/parts/home/Advertise.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Advertise = () => {
+    return (
+        <section className="advertise-section">
+			<div className="container">
+				<div className="advertise-box">
+					<a href={false}>
+                        <img src="upload/banners/ban5.jpg" alt="" />
+                    </a>
+				</div>
+			</div>
+		</section>
+    );
+}
+ 
+export default Advertise;
--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from '../parts/Header';
+import Slider from '../parts/home/Slider';
+import TrendingPosts from '../parts/home/TrendingPosts';
+import FreshStories from '../parts/home/FreshStories';
+import Advertise from '../parts/home/Advertise';
+import Articles from '../parts/home/Articles';
 import Footer from '../parts/Footer';
 
 const Home = () => {
     return (
         <>
             <Header />
-                Hello World!
+                <Slider />
+                <TrendingPosts />
+                <FreshStories />
+                <Advertise />
+                <Articles />
             <Footer />
         </>
     );
