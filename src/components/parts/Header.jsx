5ed42dea40eadd37f0343ab2e4320f5236b81438
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-16 16:00:54 +0800

    Develop navbar links and pages

--- src/components/parts/Header.jsx
+++ src/components/parts/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Header = () => {
     return (
@@ -13,41 +13,7 @@ const Header = () => {
 					</div>
 				</div>
 			</div>
-
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<div className="container">
-
-					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<a className="search-button" href="false"><i className="fa fa-search"></i></a>
-						<form className="form-search">
-							<input type="search" placeholder="Search:"/>
-						</form>
-						<ul className="navbar-nav m-auto">
-							<li className="drop-link">
-								<Link to="/" className="active">Home</Link>
-							</li>
-							<li className="drop-link">
-								<Link to="/posts">Posts</Link>
-							</li>
-							<li>
-								<Link to="/about">About</Link>
-							</li>
-							<li>
-								<Link to="/contact">Contact</Link>
-							</li>
-						</ul>
-						<ul className="navbar-nav ml-auto social-list">
-							<li><a href="false"><i className="fa fa-facebook"></i></a></li>
-							<li><a href="false"><i className="fa fa-twitter"></i></a></li>
-							<li><a href="false"><i className="fa fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+			<Navbar />
 		</header>
     );
 }
--- src/components/screens/SinglePost.jsx
+++ src/components/screens/SinglePost.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../parts/Header';
 import Footer from '../parts/Footer';
+import Slider from '../parts/home/Slider';
 
 const SinglePost = () => {
     return (
         <>
             <Header />
+                <Slider />
                 <h1>SINGLEPOST</h1>
             <Footer />
         </>
