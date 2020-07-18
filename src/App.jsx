9401ce40153814d60a83459576e601017cdd1905
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-18 13:57:30 +0800

    Customize 404 not found ui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/screens/Home';
 import Posts from './components/screens/Posts';
 import SinglePost from './components/screens/SinglePost';
 import About from './components/screens/About';
 import Contact from './components/screens/Contact';
+import NotFound from './components/screens/NotFound';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route path='/' exact>
-          <Home />
-        </Route>
-        <Route path='/posts'>
-          <Posts />
-        </Route>
-        <Route path='/single-post'>
-          <SinglePost />
-        </Route>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/contact'>
-          <Contact />
-        </Route>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/posts'>
+            <Posts />
+          </Route>
+          <Route path='/single-post'>
+            <SinglePost />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
